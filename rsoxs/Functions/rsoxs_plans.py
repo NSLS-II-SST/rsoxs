f759972c3c9aa1b6a80287da77721749726b8c2d
Eliot Gann <egann@bnl.gov>
2023-01-06 19:25:39 -0500

    adding in nicer queue user interaction

--- rsoxs/Functions/rsoxs_plans.py
+++ rsoxs/Functions/rsoxs_plans.py
@@ -1,6 +1,7 @@
 import bluesky.plan_stubs as bps
+import datetime
 from copy import deepcopy
-from sst_funcs.printing import run_report
+from sst_funcs.printing import run_report,boxed_text
 from rsoxs_scans.acquisition import dryrun_bar
 from rsoxs_scans.spreadsheets import save_samplesxls
 from rsoxs_scans.rsoxs import dryrun_rsoxs_plan
@@ -10,8 +11,13 @@ from ..HW.lakeshore import tem_tempstage
 from ..HW.signals import High_Gain_diode_i400, setup_diode_i400
 from .energyscancore import NEXAFS_fly_scan_core, new_en_scan_core
 from ..startup import RE
+from ..HW.slackbot import rsoxs_bot
 
 run_report(__file__)
+
+def time_sec(seconds):
+    dt = datetime.timedelta(seconds=seconds)
+    return str(dt).split(".")[0]
 
 actions = {
     'load_configuration', # high level load names RSoXS configuration
@@ -45,9 +51,15 @@ def run_bar(bar,
     if dry_run:
         return None
     print("Starting Queue")
-    for queue_step in queue:
-        yield from run_queue_step(queue_step)
-        if delete_as_complete:
+    queue_start_time = datetime.datetime.now()
+    acq_step = queue[0]['acq_step']
+    new_acq = False
+    acq_time = False
+    total_time = False
+    cummulative_time = False
+    time_after = False
+    for i,queue_step in enumerate(queue):
+        if delete_as_complete and queue_step['acq_step'] > acq_step:
             for samp in bar:
                 for i,acq in enumerate(samp['acquisitions']):
                     if acq['uuid'] == queue_step['uuid']:
@@ -57,14 +69,44 @@ def run_bar(bar,
             if len(save_each_step)>0:
                 save_samplesxls(bar,save_each_step)
                 print(f'saved xslx file to {save_each_step}')
-    print("End of Queue")
+        if queue_step['acq_step'] > acq_step: # new acquisition
+            if (acq_time):# only true if this isn't the first step - means we just finished a step
+                message = f'Finished.  Took {time_sec(acq_time)} \n'
+                message +=f'total time {time_sec(total_time)}, expected {time_sec(cummulative_time)}\n'
+                message +=f'expected time remaining {time_sec(time_after)}\n'
+            else:
+                message = ""
+            
+            message += f"Starting step {queue_step['queue_step']} of acquisition #{queue_step['acq_index']} of {queue_step['total_acq']} total\n"
+            message +=f"\nwhich should take {time_sec(queue_step['acq_time'])}\n"
+            message +=queue_step['description']
+            rsoxs_bot.send_message(message)
+            boxed_text('queue status',message,"red",width=120,shrink=True)
+            start_time = datetime.datetime.now()
+
+        acq_step = queue_step['acq_step']
+        
+        yield from run_queue_step(queue_step)
+        
+        acq_time = datetime.datetime.now() - start_time
+        total_time = datetime.datetime.now() - queue_start_time
+        cummulative_time = queue_step["cummulative_time"]
+        time_after = queue_step["time_after"]
+        
+    
+    total_time = datetime.datetime.now() - queue_start_time
+    message = f'Finished.  Took {time_sec(acq_time)} \n'
+    message +=f'total time {time_sec(total_time)}, expected {time_sec(cummulative_time)}\n'
+    message +=f'End of Queue'
+    rsoxs_bot.send_message(message)
+    boxed_text('queue status',message)
     return None
 
 def run_queue_step(step):
     if step['queue_step']<1: # we haven't seen a second queue step, so we don't mention it
-        print(f"\n----- starting acquisition step {step['acq_step']}-----\n")
+        print(f"\n----- starting queue step {step['queue_step']}-----\n")
     else:
-        print(f"\n----- starting acquisition step {step['acq_step']} in queue step {step['queue_step']}-----\n")
+        print(f"\n----- starting queue step {step['queue_step']} in acquisition # {step['acq_index']}-----\n")
     print(step['description'])
     if step['action'] == 'diode_low':
         yield from setup_diode_i400()
@@ -93,7 +135,10 @@ def run_queue_step(step):
         yield from NEXAFS_fly_scan_core(**step['kwargs'])
     if step['action'] == 'rsoxs_scan_core':
         yield from new_en_scan_core(**step['kwargs'])
-    print(f"\n----- finished acquisition step {step['acq_step']} in queue step {step['queue_step']}-----\n")
+    if step['acq_index']<1:
+        print(f"\n----- finished queue step {step['queue_step']}-----\n")
+    else:
+        print(f"\n----- finished queue step {step['queue_step']} in acquisition # {step['acq_index']}-----\n")
     
 # plans for manually running a single rsoxs scan in the terminal or making your own plans
 def do_rsoxs(md=RE.md,**kwargs):
