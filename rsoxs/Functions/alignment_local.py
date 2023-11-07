new rotation stage, flyscanning, beamtime fixes

rsoxs/Functions/alignment_local.py
@@ -94,7 +94,7 @@ def sanatize_angle(samp, force=False):
         if samp["front"]:
             if goodnumber:
                 samp["bar_loc"]["th"] = float(np.mod(345 - np.mod(90 + samp["angle"] + 3600, 180) + 90, 360) - 165)
-                if samp["bar_loc"]["x0"] < -1.8 and np.abs(samp["angle"]) > 30:
+                if samp["bar_loc"]["x0"] > 6 and np.abs(samp["angle"]) > 30:
                     # transmission from the left side of the bar at a incident angle more than 20 degrees,
                     # flip sample around to come from the other side - this can take a minute or two
                     samp["bar_loc"]["th"] = float(
@@ -110,7 +110,7 @@ def sanatize_angle(samp, force=False):
         else:
             if goodnumber:
                 samp["bar_loc"]["th"] = float(np.mod(90 + samp["angle"] + 3600, 180) - 90)
-                if samp["bar_loc"]["x0"] > -1.8 and np.abs(samp["angle"]) > 30:
+                if samp["bar_loc"]["x0"] < -5 and np.abs(samp["angle"]) > 30: #TODO figure out the geometry constraints for the back of the bar transmission
                     # transmission from the right side of the bar at a incident angle more than 20 degrees,
                     # flip to come from the left side
                     samp["bar_loc"]["th"] = float(np.mod(90 - samp["angle"] + 3600, 180) - 90.0)
@@ -127,6 +127,7 @@ def sanatize_angle(samp, force=False):
 
 
 def samp_dict_from_id_or_num(num_or_id):
+    rsoxs_config.read()
     if isinstance(num_or_id,str):
         sam_dict = [d for (index, d) in enumerate(rsoxs_config['bar']) if d['sample_id'].find(num_or_id) >= 0]
         if len(sam_dict) > 0:
@@ -601,8 +602,8 @@ def zoffset(af1zoff, af2zoff, y, front=True, height=0.25, af1y=-186.3, af2y=4):
 
     # offset the line by the front/back offset + height
     if front:
-        #return z0 - 2.5 - height # TODO: fix this 2.5 offset number - seems to be wrong perhaps needs to be on back?
-        return z0 - 4.5 - height # TODO: fix this 2.5 offset number - seems to be wrong perhaps needs to be on back?
+        #return z0 - 2.5 - height # 
+        return z0 + 4.5 - height # fixed Nov 2023 with new rotation stage
     else:
         return z0 + height
     # return the offset intersect
@@ -724,7 +725,7 @@ def resolve_spirals(bar=None):
                 im_num = input(f"sample {samp['sample_name']} scan {h['start']['scan_id']} which image number (or numbers, seperated by commas) is/are best?  ")
                 for i,im in enumerate(im_num.split(",")):
                     im = int(im)
-                    if i>1:
+                    if i>0:
                         accept = input(f"duplicate {samp['sample_name']} to a new sample with position {im} at ({xs[im],ys[im]}) (y,n)?")
                         if accept in ['y','Y','yes']:
                             newsamp =deepcopy(samp)
@@ -736,7 +737,8 @@ def resolve_spirals(bar=None):
                                                 'best_num':im}
                             newsamp['sample_name']+=f'_{i}'
                             newsamp['sample_id']+=f'_{i}'
-                            bar.append(newsamp)
+                            rsoxs_config['bar'].append(newsamp)
+                            rsoxs_config.write()
                     else:
                         accept = input(f"image {im} at ({xs[im],ys[im]}) is correct (y,n)?")
                         if accept in ['y','Y','yes']:
@@ -746,7 +748,8 @@ def resolve_spirals(bar=None):
                                                 {'motor':'z','position':z}]
                             samp['bar_loc']['spiral_done']={"scan":h['start']['uid'],
                                                 'best_num':im}
-    rsoxs_config.write()
+                            rsoxs_config.write()
+    
 
 
 def clear_bar():

rsoxs/Functions/common_procedures.py
@@ -67,8 +67,8 @@ def buildeputable(
     ensout = []
     heights = []
     heightsbs = []
-    Izero_Mesh.kind = "hinted"
-    Beamstop_SAXS.kind = "hinted"
+    #Izero_Mesh.kind = "hinted"
+    #Beamstop_SAXS.kind = "hinted"
     mono_en.kind = "hinted"
     epu_gap.kind = "hinted"
     # startinggap = epugap_from_energy(ens[0]) #get starting position from existing table
@@ -78,7 +78,7 @@ def buildeputable(
     #     yield from grating_to_250(2.0,-6.3,0.0)
     # elif grat == "rsoxs":
     #     yield from grating_to_rsoxs(2.0,-6.3,0.0)
-    bec.enable_plots()
+    # bec.enable_plots()
 
     plt.close()
     plt.close()
@@ -124,7 +124,7 @@ def buildeputable(
             epu_gap,
             startgap,
             endgap,
-            [None],
+            [200],
             10,
             True,
             True,
@@ -193,22 +193,26 @@ def do_some_eputables_2023_en():
         return energy+5
 
     #angles = np.linspace(18,10,4)
-    up = list(np.geomspace(0.5,45,6))
-    down = list(90-np.geomspace(0.5,40,6))
-    down.reverse()
-    angles = [0,90] + up + down
+    #up = list(np.geomspace(0.5,45,9))
+    #down = list(90-np.geomspace(0.5,40,9))
+    #down.reverse()
+    #angles = [0,90] + up + down
     # linear polarizations 0-90
-    for angle in angles:
-        yield from buildeputable(starting_energy(angle), 1300, 20, 3, 14000, phase_from_angle(angle), "L", "rsoxs", f"linear_{angle}deg_r5")
+    #for angle in angles:
+    for angle in [0,90]:
+        yield from buildeputable(starting_energy(angle)*3, 2200, 50, 3, 14000, phase_from_angle(angle), "L", "1200", f"linear3x_{angle}deg_r5")
     # circular polarizations
-    yield from buildeputable(100, 1300, 20, 3, 14000, 15000, "C", "rsoxs", f"Circ_r5")
-    yield from buildeputable(100, 1300, 20, 3, 14000, 15000, "CW", "rsoxs", f"CWCirc_r5")
+    yield from buildeputable(300, 2200, 50, 3, 14000, 15000, "C", "1200", f"Circ3_r5")
+    yield from buildeputable(300, 2200, 50, 3, 14000, 15000, "CW", "1200", f"CWCirc3_r5")
+
+    for angle in [30,60,45,10,80]:
+        yield from buildeputable(starting_energy(angle)*3, 2200, 50, 5, 14000, phase_from_angle(angle), "L", "1200", f"linear3x_{angle}deg_r5")
     
-    angles = up + down
+    #angles = up + down
 
     # linear polarizations 90-180
-    for angle in angles:
-        yield from buildeputable(starting_energy(angle), 1300, 20, 3, 14000, phase_from_angle(angle), "L3", "rsoxs", f"linear_{180-angle}deg_r6")
+    #for angle in [0,15,30,45,60,75,85]:
+    #    yield from buildeputable(starting_energy(angle), 1300, 20, 3, 14000, phase_from_angle(angle), "L3", "rsoxs", f"linear_{180-angle}deg_r6")
 
 
     #third harmonics
@@ -216,7 +220,7 @@ def do_some_eputables_2023_en():
 
     # # linear polarizations 0-90
     # for angle in angles:
-    #     yield from buildeputable(starting_energy(angle)*3, 2200, 30, 5, 14000, phase_from_angle(angle), "L", "1200", f"linear3_{angle}deg_r5")
+    #     yield from buildeputable(1000, 2200, 50, 5, 14000, phase_from_angle(angle), "L", "1200", f"linear3_{angle}deg_r5")
 
     
     # angles = up + down