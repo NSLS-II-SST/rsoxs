bbfc8d0dc8ba9062406f5d94a707ec810fa68bd5
Charles Titus <ctitus@bnl.gov>
2025-01-16 21:19:04 -0500

    Reorganizig folders somewhat, mainly adding rsoxs plan loading from configuration toml file, and preparing for loading queues into the GUI from a spreadsheet

---
+++ rsoxs/plans/rsoxs.py
@@ -0,0 +1,159 @@
+import bluesky.plan_stubs as bps
+from bluesky.preprocessors import finalize_wrapper
+from functools import partial
+from nbs_bl.hw import (
+    en,
+    Shutter_control,
+    waxs_det,
+)
+
+from nbs_bl.printing import run_report
+from nbs_bl.plans.scans import nbs_list_scan, nbs_gscan
+from nbs_bl.plans.preprocessors import wrap_metadata
+from nbs_bl.utils import merge_func
+from nbs_bl.queueserver import GLOBAL_USER_STATUS
+from nbs_bl.help import _add_to_import_list, add_to_plan_list, add_to_func_list
+
+from .per_steps import take_exposure_corrected_reading, one_nd_sticky_exp_step
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+run_report(__file__)
+
+GLOBAL_RSOXS_PLANS = GLOBAL_USER_STATUS.request_status_dict("RSOXS_PLANS", use_redis=True)
+GLOBAL_RSOXS_PLANS.clear()
+
+
+def add_to_rsoxs_list(f, key, **plan_info):
+    """
+    A function decorator that will add the plan to the built-in list
+    """
+    _add_to_import_list(f, "rsoxs")
+    GLOBAL_RSOXS_PLANS[key] = {}
+    GLOBAL_RSOXS_PLANS[key].update(plan_info)
+    return f
+
+
+@add_to_plan_list
+@merge_func(nbs_gscan, use_func_name=False, omit_params=["motor"])
+def variable_energy_scan(*args, **kwargs):
+    yield from bps.mv(Shutter_control, 1)
+    yield from finalize_wrapper(plan=nbs_gscan(en.energy, *args, **kwargs), final_plan=post_scan_hardware_reset())
+
+
+@add_to_plan_list
+@merge_func(variable_energy_scan, use_func_name=False, omit_params=["per_step"])
+def rsoxs_step_scan(*args, extra_dets=[], n_exposures=1, **kwargs):
+    """
+    Step scanned RSoXS function with WAXS Detector
+
+    Parameters
+    ----------
+    n_exposures : int, optional
+        If greater than 1, take multiple exposures per step
+    """
+    old_n_exp = waxs_det.number_exposures
+    waxs_det.number_exposures = n_exposures
+    _extra_dets = [waxs_det]
+    _extra_dets.extend(extra_dets)
+    rsoxs_per_step = partial(
+        one_nd_sticky_exp_step,
+        take_reading=partial(take_exposure_corrected_reading, shutter=Shutter_control, check_exposure=False),
+    )
+    yield from variable_energy_scan(*args, extra_dets=_extra_dets, per_step=rsoxs_per_step, **kwargs)
+    waxs_det.number_exposures = old_n_exp
+
+
+@add_to_plan_list
+@merge_func(nbs_list_scan, use_func_name=False, omit_params=["*args"])
+def energy_step_scan(energies, **kwargs):
+    yield from bps.mv(Shutter_control, 1)
+    yield from finalize_wrapper(
+        plan=nbs_list_scan(en.energy, energies, **kwargs), final_plan=post_scan_hardware_reset()
+    )
+
+
+def post_scan_hardware_reset():
+    ## Make sure the shutter is closed, and the scanlock if off after a scan, even if it errors out
+    yield from bps.mv(en.scanlock, 0)
+    yield from bps.mv(Shutter_control, 0)
+
+
+def _wrap_rsoxs(element, edge):
+    def decorator(func):
+        return wrap_metadata({"element": element, "edge": edge, "scantype": "rsoxs"})(func)
+
+    return decorator
+
+
+def _rsoxs_factory(energy_grid, element, edge, key):
+    @_wrap_rsoxs(element, edge)
+    @wrap_metadata({"plan_name": key})
+    @merge_func(rsoxs_step_scan, omit_params=["args"])
+    def inner(**kwargs):
+        """Parameters
+        ----------
+        repeat : int
+            Number of times to repeat the scan
+        **kwargs :
+            Arguments to be passed to tes_gscan
+
+        """
+
+        yield from rsoxs_step_scan(*energy_grid, **kwargs)
+
+    d = f"Perform an in-place RSoXS scan for {element} with energy pattern {energy_grid} \n"
+    inner.__doc__ = d + inner.__doc__
+
+    inner.__qualname__ = key
+    inner.__name__ = key
+    inner._edge = edge
+    inner._short_doc = f"Do RSoXS for {element} from {energy_grid[0]} to {energy_grid[-2]}"
+    return inner
+
+
+@add_to_func_list
+def load_rsoxs(filename):
+    """
+    Load RSoXS plans from a TOML file and inject them into the IPython user namespace.
+
+    Parameters
+    ----------
+    filename : str
+        Path to the TOML file containing XAS plan definitions
+    """
+    try:
+        # Get IPython's user namespace
+        ip = get_ipython()
+        user_ns = ip.user_ns
+    except (NameError, AttributeError):
+        # Not running in IPython, just return the generated functions
+        user_ns = None
+
+    generated_plans = {}
+    with open(filename, "rb") as f:
+        regions = tomllib.load(f)
+        for _key, value in regions.items():
+            if "xas" in _key:
+                key = _key.replace("xas", "rsoxs")
+            else:
+                key = _key
+            name = value.get("name", key)
+            region = value.get("region")
+            element = value.get("element", "")
+            edge = value.get("edge", "")
+            rsoxs_func = _rsoxs_factory(region, element, edge, key)
+            add_to_rsoxs_list(rsoxs_func, key, name=name, element=element, edge=edge, region=region)
+
+            # Store the function
+            generated_plans[key] = rsoxs_func
+
+            # If we're in IPython, inject into user namespace
+            if user_ns is not None:
+                user_ns[key] = rsoxs_func
+
+    # Return the generated plans dictionary in case it's needed
+    return generated_plans
---
+++ rsoxs/qt/plans/rsoxs_plan_loader.py
@@ -0,0 +1,77 @@
+from qtpy.QtWidgets import QMessageBox
+from qtpy.QtCore import Signal
+from bluesky_queueserver_api import BPlan
+from nbs_gui.plans.planLoaders import PlanLoaderWidgetBase
+
+
+class RSOXSPlanLoader(PlanLoaderWidgetBase):
+    display_name = "RSoXS Plans"
+    signal_update_rsoxs = Signal(object)
+    signal_update_samples = Signal(object)
+
+    def __init__(self, model, parent=None):
+        super().__init__(model, parent)
+        self.rsoxs_plans = {}
+        self.signal_update_rsoxs.connect(self.update_rsoxs)
+        self.user_status.register_signal("RSOXS_PLANS", self.signal_update_rsoxs)
+
+        self.signal_update_samples.connect(self.update_samples)
+        self.user_status.register_signal("GLOBAL_SAMPLES", self.signal_update_samples)
+
+    def update_rsoxs(self, rsoxs_plans):
+        self.rsoxs_plans = rsoxs_plans
+
+    def update_samples(self, sample_dict):
+        self.samples = sample_dict
+
+    def get_plan(self, plan_name):
+        plan_name = plan_name.lower()
+        if plan_name in self.rsoxs_plans:
+            return plan_name
+        else:
+            for plan_key, plan_info in self.rsoxs_plans.items():
+                if plan_name in [
+                    plan_info.get("name", "").lower(),
+                    plan_info.get("edge", "").lower(),
+                ]:
+                    return plan_key
+        raise KeyError(f"{plan_name} not found in list of RSoXS Plans")
+
+    def create_plan_items(self):
+        items = []
+        for plan_data in self.plan_queue_data:
+            sample_id = plan_data.get("Sample ID")
+            plan_name = plan_data.get("Edge")
+
+            try:
+                plan = self.get_plan(plan_name)
+            except Exception as e:
+                QMessageBox.critical(
+                    self,
+                    "Plan Generation Error",
+                    f"Failed to create plan: {str(e)}",
+                    QMessageBox.Ok,
+                )
+                return []
+
+            if sample_id not in self.samples:
+                QMessageBox.critical(
+                    self,
+                    "Plan Generation Error",
+                    f"Sample: {sample_id} not in sample list",
+                    QMessageBox.Ok,
+                )
+                return []
+
+            plan_kwargs = {}
+            plan_kwargs["eslit"] = (
+                float(plan_data.get("Slit Size", None)) if plan_data.get("Slit Size") is not None else None
+            )
+            plan_kwargs["group_name"] = plan_data.get("Group Name", None)
+            plan_kwargs["comment"] = plan_data.get("Comment", None)
+            plan_kwargs["repeat"] = int(plan_data.get("Repeat", 1))
+            plan_kwargs["n_exposures"] = int(plan_data.get("Exposures", 1))
+
+            item = BPlan(plan, sample=sample_id, **plan_kwargs)
+            items.append(item)
+        return items
