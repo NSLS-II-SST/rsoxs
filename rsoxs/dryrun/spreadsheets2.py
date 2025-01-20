6c220ed3d8b1ea1a444df82353c62aaff2a361ce
Eliot Gann <egann@bnl.gov>
2025-01-19 22:21:20 -0500

    Made a simple function to load a spreadsheet and update rsoxs_config

--- rsoxs/dryrun/spreadsheets2.py
+++ rsoxs/dryrun/spreadsheets2.py
@@ -1,7 +1,40 @@
 ##
 
-
+import pandas as pd
 from ..startup import rsoxs_config
 
-def load_spreadsheet(path):
+Keys_SamplesDictionary = ["sample_id",
+                          "proposal_id",
+                          "notes"]
+Keys_AcquisitionsDictionary = ["sample_id",
+                               "plan_name",
+                               "energy_parameters",
+                               "notes"]
+
+def load_spreadsheet(path, update_configuration = False):
+    """
+    Loads excel spreadsheet and loads sample metadata into memory.
+    """
     
+    Samples_df = pd.read_excel(path, sheet_name="Samples")
+    Samples_dict = Samples_df.to_dict(orient="records")
+    Configuration = []
+    for Index, Sample in enumerate(Samples_dict):
+        Sample_dict = {}
+        for Index, Key in enumerate(Keys_SamplesDictionary): Sample_dict[Key] = Sample[Key]
+        ## TODO: sanitize sample parameters
+        Sample_dict["acquisitions"] = []
+        Configuration.append(Sample_dict)
+
+    Acquisitions_df = pd.read_excel(path, sheet_name="Acquisitions")
+    Acquisitions_dict = Acquisitions_df.to_dict(orient="records")
+    for Index, Acquisition in enumerate(Acquisitions_dict):
+        Acquisitions_dict = {}
+        for Index, Key in enumerate(Keys_AcquisitionsDictionary): Acquisitions_dict[Key] = Acquisition[Key]
+        ## TODO: sanitize acquisition parameters
+        for Index, Sample in enumerate(Configuration):
+            if Sample["sample_id"] == Acquisitions_dict["sample_id"]: Configuration[Index]["acquisitions"].append(Acquisitions_dict)
+    
+    ## TODO: validate spreadsheet version?  Probably unnecessary if sanitized correctly?
+
+    if update_configuration: rsoxs_config["bar"] = Configuration
